bf60b8b32a86737c7f49dbb76c783e648709419a
EvanoVickyZ <vickyzachs.dev@gmail.com>
2026-04-24 17:51:56 +0800

    fixing progress 2 : try to debuging data.js

--- api/data.js
+++ api/data.js
@@ -3,36 +3,51 @@ const { fetchDatabase } = require("../services/notionService");
 const { profileTransform } = require("../transform/profileTransform");
 const { projectTransform } = require("../transform/projectTransform");
 
-module.exports = async (req, res) => {
+// module.exports = async (req, res) => {
+//   try {
+//     console.log("Vercel /data hit");
+
+//     const profileRaw = await fetchDatabase(notionConfig.profileDbId);
+//     const projectRaw = await fetchDatabase(notionConfig.projectDbId);
+
+//     const profile = profileRaw?.length
+//       ? profileTransform(profileRaw[0])
+//       : {};
+
+//     const projects = Array.isArray(projectRaw)
+//       ? projectRaw.map(projectTransform)
+//       : [];
+
+//     const finalData = {
+//       profile,
+//       projects,
+//       version: Date.now(),
+//       lastUpdated: new Date().toISOString(),
+//     };
+
+//     return res.status(200).send("WORKING");
+
+//     // return res.status(200).json(finalData);
+
+//   } catch (err) {
+//     console.error("API ERROR:", err);
+//     return res.status(500).json({
+//       error: "failed to fetch data"
+//     });
+//   } };
+module.exports = (req, res) => {
   try {
-    console.log("Vercel /data hit");
-
-    const profileRaw = await fetchDatabase(notionConfig.profileDbId);
-    const projectRaw = await fetchDatabase(notionConfig.projectDbId);
-
-    const profile = profileRaw?.length
-      ? profileTransform(profileRaw[0])
-      : {};
-
-    const projects = Array.isArray(projectRaw)
-      ? projectRaw.map(projectTransform)
-      : [];
-
-    const finalData = {
-      profile,
-      projects,
-      version: Date.now(),
-      lastUpdated: new Date().toISOString(),
-    };
-
-    return res.status(200).send("WORKING");
-
-    // return res.status(200).json(finalData);
-
+    return res.status(200).json({
+      ok: true,
+      env: {
+        hasToken: !!process.env.NOTION_TOKEN,
+        hasProfileDB: !!process.env.NOTION_PROFILE_DB_ID,
+        hasProjectDB: !!process.env.NOTION_PROJECT_DB_ID,
+      }
+    });
   } catch (err) {
-    console.error("API ERROR:", err);
     return res.status(500).json({
-      error: "failed to fetch data"
+      error: err.message,
     });
   }
 };
