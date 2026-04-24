c17cfb4f58fb47794a781f70c6950d1e1b24ba12
EvanoVickyZ <vickyzachs.dev@gmail.com>
2026-04-24 18:03:26 +0800

    fixing progress 4 : testing notion logic

--- api/data.js
+++ api/data.js
@@ -1,6 +1,19 @@
-module.exports = (req, res) => {
-  return res.status(200).json({
-    ok: true,
-    message: "API working"
-  });
+const { fetchDatabase } = require("../services/notionService");
+const notionConfig = require("../config/notionConfig");
+
+module.exports = async (req, res) => {
+  try {
+    const profileRaw = await fetchDatabase(notionConfig.profileDbId);
+
+    return res.status(200).json({
+      ok: true,
+      count: profileRaw?.length || 0,
+      sample: profileRaw?.[0] || null
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message
+    });
+  }
 };
