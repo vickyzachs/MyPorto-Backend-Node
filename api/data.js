6bf58258384bb0372502628109842ee665b5ba41
EvanoVickyZ <vickyzachs.dev@gmail.com>
2026-04-24 18:01:03 +0800

    fixing progress 3 : fixing data.js

--- api/data.js
+++ api/data.js
@@ -1,42 +1,6 @@
-const { fetchDatabase } = require("../services/notionService");
-const notionConfig = require("../config/notionConfig");
-const transformProfile = require("../transform/profileTransform");
-const { projectTransform } = require("../transform/projectTransform");
-
-module.exports = async (req, res) => {
-  try {
-    console.log("API HIT");
-
-    if (!process.env.NOTION_TOKEN) {
-      throw new Error("Missing NOTION_TOKEN in Vercel env");
-    }
-
-    const profileRaw = await fetchDatabase(notionConfig.profileDbId);
-    const projectRaw = await fetchDatabase(notionConfig.projectDbId);
-
-    console.log("PROFILE RAW:", profileRaw);
-    console.log("PROJECT RAW:", projectRaw?.length);
-
-    const profile = profileRaw?.length
-      ? transformProfile(profileRaw[0])
-      : {};
-
-    const projects = Array.isArray(projectRaw)
-      ? projectRaw.map(projectTransform)
-      : [];
-
-    return res.status(200).json({
-      profile,
-      projects,
-      version: Date.now(),
-    });
-
-  } catch (err) {
-    console.error("CRASH ERROR:", err);
-
-    return res.status(500).json({
-      error: err.message,
-      stack: err.stack,
-    });
-  }
+module.exports = (req, res) => {
+  return res.status(200).json({
+    ok: true,
+    message: "API working"
+  });
 };
